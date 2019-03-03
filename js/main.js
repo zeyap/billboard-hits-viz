114a0cc8215b148864707cc04f28488afc987be2
zeyap <zp82@cornell.edu>
2019-03-03 15:17:01 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+d3.csv('../static/Hot Stuff.csv')
+.then(data=>{
+    var dataByYear = [];
+    var dataByPerformers = {};
+    var dataBy5Years = [];
+    const latestYear = 2019;
+    let minYear = latestYear;
+
+    //find min year & organize data by performer
+    data.forEach(elem=>{
+        let year = parseInt(elem.WeekID.split('/')[2]);
+        minYear = Math.min(latestYear, year);
+        dataByYear[year] = dataByYear[year]||[];
+        dataByYear[year].push(elem);
+
+        dataByPerformers[elem.Performer][year] = dataByPerformers[elem.Performer][year]||[];
+        dataByPerformers[elem.Performer][year].push(elem);
+    });
+
+    //data by 5 year
+    for (let year in dataByYear){
+        if(dataByYear[year] === undefined)continue;
+        let period = Math.floor((parseInt(year)-minYear)/5);
+        dataBy5Years[period] = (dataBy5Years[period]||[]).concat(dataByYear[year]);
+    }
+
+    console.log(dataByPerformers)
+
+    console.log(dataBy5Years)
+    
+
+})
+
